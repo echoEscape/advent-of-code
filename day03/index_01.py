a9abce30380bb32936efdf44b28fa6a67417d7c9
echoEscape <thenumberiszero@gmail.com>
2023-12-08 23:26:55 +0100

    Fixed errors in the logic of checking all directions for a number

--- day03/index_01.py
+++ day03/index_01.py
@@ -12,28 +12,29 @@ for y in range(len(lines)):
         
         #Check if symbol
         if lines[y][x] != "." and lines[y][x] not in numbers:
-            #Check if a number is around the symbol
+            #Check if a number is out of bounds around the checked pos
             #up
-            if y-1 > 0 and lines[y-1][x] in numbers:
-                pass
-            #down
-            if y+1 < len(lines-1) and lines[y+1][x] in numbers:
+            if y-1 >= 0 and lines[y-1][x] in numbers:
                 pass
             #left
-            if x-1 > 0 and lines[y][x-1] in numbers:
+            if x-1 >= 0 and lines[y][x-1] in numbers:
+                pass
+            #down
+            if y+1 < len(lines[y])-1 and lines[y+1][x] in numbers:
                 pass
             #right
-            if x+1 < len(lines[y-1]) and lines[y][x+1] in numbers:
+            if x+1 < len(lines[y][x])-1 and lines[y][x+1] in numbers:
                 pass
-            #top left
-            if y-1 > 0 and lines[y-1][x] in numbers and x-1 > 0 and lines[y][x-1] in numbers:
+            #up-left
+            if y-1 >= 0 and x-1 >= 0 and lines[y-1][x-1] in numbers:
                 pass
-            #top right
-            if y-1 > 0 and lines[y-1][x] in numbers and x+1 < len(lines[y-1]) and lines[y][x+1] in numbers:
+            #up-right
+            if y-1 >= 0 and x+1 < len(lines[y][x])-1 and lines[y-1][x+1] in numbers:
                 pass
-            #bottom left
-            if y+1 < len(lines-1) and lines[y+1][x] in numbers and x-1 > 0 and lines[y][x-1] in numbers:
+            #down-left
+            if y+1 < len(lines[y])-1 and x-1 >= 0 and lines[y+1][x-1] in numbers:
                 pass
-            #bottom right
-            if y+1 < len(lines-1) and lines[y+1][x] in numbers and x+1 < len(lines[y-1]) and lines[y][x+1] in numbers:
+            #down-right
+            if y+1 < len(lines[y])-1 and x+1 < len(lines[y][x])-1 and lines[y+1][x+1] in numbers:
                 pass
+
