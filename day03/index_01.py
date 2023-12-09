1451a48d670713c2461f3e499fa54529fa682b3d
echoEscape <thenumberiszero@gmail.com>
2023-12-09 15:45:22 +0100

    Changed check to elif to stop multiple matches

--- day03/index_01.py
+++ day03/index_01.py
@@ -19,8 +19,8 @@ def get_fullNumber(line, checked_x_pos, numbers):
     while end_x < len(line) and line[end_x+1] in numbers:
         end_x += 1
 
-    print("---")
     print(line[:start_x] + "\x1b[6;30;42m" + line[start_x:end_x+1] + "\x1b[0m" + line[end_x+1:])
+    print("---")
     #Correct offset to include last digit
     end_x = end_x + 1
     return line[start_x:end_x]
@@ -32,38 +32,38 @@ for y in range(len(lines)-1):
         #Check if symbol
         if lines[y][x] != "." and lines[y][x] not in numbers:
             #Check if a number is out of bounds around the checked pos
-            print(lines[y][:x] + "\033[91m" + lines[y][x] + "\033[0m" + lines[y][x+1])
+            print(lines[y][:x] + "\033[91m" + lines[y][x] + "\033[0m" + lines[y][x+1:])
             #up
             if y-1 >= 0 and lines[y-1][x] in numbers:
-                print("up")
                 solutionSum += int(get_fullNumber(lines[y-1], x, numbers))
+                print("up")
             #left
-            if x-1 >= 0 and lines[y][x-1] in numbers:
-                print("left")
+            elif x-1 >= 0 and lines[y][x-1] in numbers:
                 solutionSum = int(get_fullNumber(lines[y], x-1, numbers))
+                print("left")
             #down
-            if y+1 < len(lines) and lines[y+1][x] in numbers:
-                print("down")
+            elif y+1 < len(lines) and lines[y+1][x] in numbers:
                 solutionSum += int(get_fullNumber(lines[y+1], x, numbers))
+                print("down")
             #right
-            if x+1 < len(lines[y]) and lines[y][x+1] in numbers:
-                print("right")
+            elif x+1 < len(lines[y]) and lines[y][x+1] in numbers:
                 solutionSum += int(get_fullNumber(lines[y], x+1, numbers))
+                print("right")
             #up-left
-            if y-1 >= 0 and x-1 >= 0 and lines[y-1][x-1] in numbers:
-                print("up-left")
+            elif y-1 >= 0 and x-1 >= 0 and lines[y-1][x-1] in numbers:
                 solutionSum += int(get_fullNumber(lines[y-1], x-1, numbers))
+                print("up-left")
             #up-right
-            if y-1 >= 0 and x+1 < len(lines[y-1]) and lines[y-1][x+1] in numbers:
-                print("up-right")
+            elif y-1 >= 0 and x+1 < len(lines[y-1]) and lines[y-1][x+1] in numbers:
                 solutionSum += int(get_fullNumber(lines[y-1], x+1, numbers))
+                print("up-right")
             #down-left
-            if y+1 < len(lines) and x-1 >= 0 and lines[y+1][x-1] in numbers:
-                print("down-left")
+            elif y+1 < len(lines) and x-1 >= 0 and lines[y+1][x-1] in numbers:
                 solutionSum += int(get_fullNumber(lines[y+1], x-1, numbers))
+                print("down-left")
             #down-right
-            if y+1 < len(lines) and x+1 < len(lines[y]) and lines[y+1][x+1] in numbers:
-                print("down-right")
+            elif y+1 < len(lines) and x+1 < len(lines[y]) and lines[y+1][x+1] in numbers:
                 solutionSum += int(get_fullNumber(lines[y+1], x+1, numbers))
+                print("down-right")
 
 print(solutionSum)
