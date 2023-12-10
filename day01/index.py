70eb0b3f09f960b416b9c74d2ec5adf690af5707
echoEscape <thenumberiszero@gmail.com>
2023-12-10 20:23:08 +0100

    Move List to corresponding functions

--- day01/index.py
+++ day01/index.py
@@ -6,7 +6,6 @@ results_pt1 = []
 results_pt2 = []
 solution_pt1 = 0
 solution_pt2 = 0
-numbername_list = ["one", "1"],["two", "2"], ["three", "3"], ["four", "4"], ["five", "5"], ["six", "6"], ["seven", "7"], ["eight", "8"], ["nine", "9"]
 
 #Split Lines of Input into its own array and remove "New line" tag
 for l in input:
@@ -18,8 +17,9 @@ def remove_characters(line):
         line = line.replace(letter, "")
     return line
 
-def convert_spelling_to_number(line, replacement_list):
-    for spelledNumber in replacement_list:
+def convert_spelling_to_number(line):
+    numbername_list = ["one", "1"],["two", "2"], ["three", "3"], ["four", "4"], ["five", "5"], ["six", "6"], ["seven", "7"], ["eight", "8"], ["nine", "9"]
+    for spelledNumber in numbername_list:
         line = line.replace(spelledNumber[0], spelledNumber[0]+spelledNumber[1]+spelledNumber[0]) #fixes the twone issue so that two can still be detected even though one gets checked first by re-adding the actual string so that it doesnt get removed
     line = remove_characters(line)
     return line
@@ -27,7 +27,7 @@ def convert_spelling_to_number(line, replacement_list):
 
 for line in lines:
     cleaned_lines_pt1.append(remove_characters(line))
-    cleaned_lines_pt2.append(convert_spelling_to_number(line, numbername_list))
+    cleaned_lines_pt2.append(convert_spelling_to_number(line))
 
 #Part 1
 for number_pt1 in cleaned_lines_pt1:
