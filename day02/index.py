d3aa0bc8704a47cb7fa75e50660613b09f79babf
echoEscape <thenumberiszero@gmail.com>
2023-12-11 18:48:36 +0100

    Build basic structure for part 2 - still incorrect solution

--- day02/index.py
+++ day02/index.py
@@ -4,13 +4,21 @@ for l in input:
     lines.append(l.replace("\n", ""))
 
 gameResults = []
+powerLevels = []
 
 for game in lines:
     game = game[game.find(": ")+2:]
     pull = game.split("; ")
 
+    #Part 1
     #If any set of number+color is False, the entire game doesnt get counted
     valid = True #Set placeholder
+
+    #Part 2
+    #Add up the power level in a loop of one game by grabbing the biggest number for each color
+    plRed = 0
+    plGreen = 0
+    plBlue = 0
     for p in pull:
         cubes = p.split(", ")
         cubeset = []
@@ -18,7 +26,7 @@ for game in lines:
         for c in cubes:
             cubeset.append(c.split(" "))
 
-        #Check Validity of amount+color ratio
+        #PART 1 - Check Validity of amount+color ratio
         for cube in cubeset:
             if cube[1] == "red" and int(cube[0]) > 12: 
                 valid = False
@@ -26,12 +34,26 @@ for game in lines:
                 valid = False
             elif cube[1] == "blue" and int(cube[0]) > 14:
                 valid = False
-
+        
+        #Part 2
+        for cube in cubeset:
+            if cube[1] == "red" and int(cube[0]) > plRed: 
+                plRed = int(cube[0])
+            elif cube[1] == "green" and int(cube[0]) > plGreen:
+                plGreen = int(cube[0])
+            elif cube[1] == "blue" and int(cube[0]) > plBlue:
+                plBlue = int(cube[0])
+        
+    #Part 1
     gameResults.append(valid)
+    #Part 2
+    powerLevels.append(plRed*plGreen*plBlue)
 
 score = 0
+plScore = 0
 for i in range(len(gameResults)):
     if gameResults[i] == True:
         score = score + (i+1)
-
+        plScore = plScore + powerLevels[i]
 print(score)
+print(plScore)
