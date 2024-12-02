db81937fb180076fd40da0329da8582748d46b33
echoEscape <thenumberiszero@gmail.com>
2024-12-02 13:40:45 +0100

    Complete Day 2 - Task 2

--- 2024/day02/day02.py
+++ 2024/day02/day02.py
@@ -11,8 +11,12 @@ for l in input:
 # 1. The values of a line are either all increasing or decreasing
 # 2. Two adjacent values of a line are allowed a difference of between 1 and 3
 # Count the correct lines
-numReports = 0
-for line in lines:
+
+# Task 2:
+# If a value causes an error it can be removed from the line and be re-checked
+# Count the correct lines with the "error dampener"
+
+def checkSafety(line):
     safe = True
 
     # Check if List is strictly decreasing or increasing
@@ -25,6 +29,7 @@ for line in lines:
         safe = False        # No difference -> diff = 0, invalid direction
 
     if safe and (sorted(line, reverse=rev) == line): # sorted() catches a change in direction within the level which would be invalid
+      # ---
       # Check if the difference of neighboring values is between 1 and 3
       for pos in range(len(line) - 1):
           diff = line[pos] - line[pos + 1]
@@ -34,6 +39,29 @@ for line in lines:
         safe = False
     
     if safe:
-        numReports += 1
+        return True
+    else:
+        return False
 
+# Task 1
+numReports = 0
+for line in lines:
+    if checkSafety(line):
+        numReports += 1
 print(numReports)
+
+#Task 2
+# Try all iterations of a line and check if one is correct, then add to sum
+numReports = 0
+for line in lines:
+    corrLevel = checkSafety(line)
+    for pos in range(len(line)):
+      tempLine = line.copy()
+      tempLine.pop(pos)
+      
+      if corrLevel == False and checkSafety(tempLine):
+          corrLevel = True
+    if corrLevel:
+        numReports += 1
+print(numReports)
+    
